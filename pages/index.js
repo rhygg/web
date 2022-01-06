44371ad13e63e3e97ec1c605fa83d21ea07828f8
tj <rhydderchc@gmail.com>
2022-01-05 23:51:29 -0600

    feat: add project cards to home page

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,35 @@
 import Head from "next/head";
-import { useColorMode, Heading, Text, Flex, Stack } from "@chakra-ui/react";
+import { useColorMode, Heading, Text, Flex, Stack, VStack, SimpleGrid, Center } from "@chakra-ui/react";
 import Container from "../components/Container";
+import ProjectCard from "../components/ProjectCard";
+const projects = [
+  {
+    key: 1,
+    name: "Spinners",
+    description:"Terminal spinners in vlang",
+    link:"https://github.com/rhygg/spinners",
+    type:"Community",
+    logo:"https://res.cloudinary.com/carloscuesta/image/upload/v1593531857/blog-featured-images/HyperTerm_a_Terminal_built_on_open_web_standards.png"
 
+  },
+  {
+    key: 2,
+    name: "UpbeatRadio",
+    description: "A community powered internet radio station!",
+    link:"https://upbeatradio.net",
+    type:"Web App",
+    logo:"https://cdn.discordapp.com/attachments/857185757394108457/913307666263191552/UB_Logo.png"
+  },
+    {
+    key: 3,
+    name: "Shiro",
+    description: "A Multipurpose discord bot used by over 20k servers!",
+    link:"https://shirobot.org",
+    type:"Discord Bot",
+    logo:"https://cdn.discordapp.com/attachments/917772093863637012/917772623964942346/Shiro4096.png"
+  },
+
+]
 export default function Index() {
   const { colorMode } = useColorMode();
   const colorSecondary = {
@@ -37,6 +65,23 @@ export default function Index() {
           </Text>
         </Flex>
       </Stack>
+       <Center as="section" w="100%">
+          <VStack align="start" spacing={8}>
+            <Heading size="lg">Projects</Heading>
+            <SimpleGrid columns={1} spacing={4} mt={8} w="100%">
+              {projects.map((project) => (
+                <ProjectCard
+                  key={project.id}
+                  name={project.name}
+                  description={project.description}
+                  logo={project.logo}
+                  link={project.link}
+                  type={project.type}
+                />
+              ))}
+            </SimpleGrid>
+          </VStack>
+          </Center>
     </Container>
   );
 }
